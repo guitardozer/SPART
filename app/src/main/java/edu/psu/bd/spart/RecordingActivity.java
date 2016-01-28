3ffab929dcd046e7917c0169799410a440bd73d1
Eugene Nitka <eugene.nitka@psu.edu>
2016-01-28 04:13:05 -0500

    Created transition from Home to Recording. Added action menu for RecordingUI.

--- app/src/main/java/edu/psu/bd/spart/RecordingActivity.java
+++ app/src/main/java/edu/psu/bd/spart/RecordingActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
 
 public class RecordingActivity extends AppCompatActivity {
@@ -26,4 +27,10 @@ public class RecordingActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_recording, menu);
+        return true;
+    }
 }
