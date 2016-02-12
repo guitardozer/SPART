94a3efc3bdf3c5c259ff03517d30046ed9aba5e5
Qwerkie <gibbiler@gmail.com>
2016-02-11 19:16:57 -0500

    Initialize Sensors (Needs Camera)

--- app/src/main/java/edu/psu/bd/spart/HomeActivity.java
+++ app/src/main/java/edu/psu/bd/spart/HomeActivity.java
@@ -1,14 +1,25 @@
 package edu.psu.bd.spart;
 
+import com.google.atap.tangoservice.Tango;
+
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.content.Intent;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+
+import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
 
--- app/src/main/java/edu/psu/bd/spart/RecordingActivity.java
+++ app/src/main/java/edu/psu/bd/spart/RecordingActivity.java
@@ -1,15 +1,49 @@
 package edu.psu.bd.spart;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.atap.tangoservice.Tango;
+import com.google.atap.tangoservice.Tango.OnTangoUpdateListener;
+import com.google.atap.tangoservice.TangoConfig;
+import com.google.atap.tangoservice.TangoCoordinateFramePair;
+import com.google.atap.tangoservice.TangoErrorException;
+import com.google.atap.tangoservice.TangoEvent;
+import com.google.atap.tangoservice.TangoOutOfDateException;
+import com.google.atap.tangoservice.TangoPoseData;
+import com.google.atap.tangoservice.TangoXyzIjData;
+
+import java.util.ArrayList;
 
 public class RecordingActivity extends AppCompatActivity {
+
+
+    private static final String TAG = HomeActivity.class.getSimpleName();
+    private static final String sTranslationFormat = "Translation: %f, %f, %f";
+    private static final String sRotationFormat = "Rotation: %f, %f, %f, %f";
+
+    private static final int SECS_TO_MILLISECS = 1000;
+    private static final double UPDATE_INTERVAL_MS = 100.0;
+
+    private double mPreviousTimeStamp;
+    private double mTimeToNextUpdate = UPDATE_INTERVAL_MS;
+
+    private TextView mTranslationTextView;
+    private TextView mRotationTextView;
+
+    private Tango mTango;
+    private TangoConfig mConfig;
+    private boolean mIsTangoServiceConnected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +58,132 @@ public class RecordingActivity extends AppCompatActivity {
             public void onClick(View view) {
                 transitModel(view);
             }
+        });
+        // Instantiate Tango client
+        mTango = new Tango(this);
+
+        // Set up Tango configuration for motion tracking
+        // If you want to use other APIs, add more appropriate to the config
+        // like: mConfig.putBoolean(TangoConfig.KEY_BOOLEAN_DEPTH, true)
+        mConfig = mTango.getConfig(TangoConfig.CONFIG_TYPE_CURRENT);
+        mConfig.putBoolean(TangoConfig.KEY_BOOLEAN_MOTIONTRACKING, true);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Lock the Tango configuration and reconnect to the service each time
+        // the app
+        // is brought to the foreground.
+        super.onResume();
+        if (!mIsTangoServiceConnected) {
+            try {
+                setTangoListeners();
+            } catch (TangoErrorException e) {
+                Toast.makeText(this, "Tango Error! Restart the app!",
+                        Toast.LENGTH_SHORT).show();
+            }
+            try {
+                mTango.connect(mConfig);
+                mIsTangoServiceConnected = true;
+            } catch (TangoOutOfDateException e) {
+                Toast.makeText(getApplicationContext(),
+                        "Tango Service out of date!", Toast.LENGTH_SHORT)
+                        .show();
+            } catch (TangoErrorException e) {
+                Toast.makeText(getApplicationContext(),
+                        "Tango Error! Restart the app!", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // When the app is pushed to the background, unlock the Tango
+        // configuration and disconnect
+        // from the service so that other apps will behave properly.
+        try {
+            mTango.disconnect();
+            mIsTangoServiceConnected = false;
+        } catch (TangoErrorException e) {
+            Toast.makeText(getApplicationContext(), "Tango Error!",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    private void setTangoListeners() {
+        // Select coordinate frame pairs
+        ArrayList<TangoCoordinateFramePair> framePairs = new ArrayList<TangoCoordinateFramePair>();
+        framePairs.add(new TangoCoordinateFramePair(
+                TangoPoseData.COORDINATE_FRAME_START_OF_SERVICE,
+                TangoPoseData.COORDINATE_FRAME_DEVICE));
+
+        // Add a listener for Tango pose data
+        mTango.connectListener(framePairs, new Tango.OnTangoUpdateListener() {
+
+            @SuppressLint("DefaultLocale")
+            @Override
+            public void onPoseAvailable(TangoPoseData pose) {
+                // Format Translation and Rotation data
+                final String translationMsg = String.format(sTranslationFormat,
+                        pose.translation[0], pose.translation[1],
+                        pose.translation[2]);
+                final String rotationMsg = String.format(sRotationFormat,
+                        pose.rotation[0], pose.rotation[1], pose.rotation[2],
+                        pose.rotation[3]);
+
+                // Output to LogCat
+                String logMsg = translationMsg + " | " + rotationMsg;
+                Log.i(TAG, logMsg);
+
+                final double deltaTime = (pose.timestamp - mPreviousTimeStamp)
+                        * SECS_TO_MILLISECS;
+                mPreviousTimeStamp = pose.timestamp;
+                mTimeToNextUpdate -= deltaTime;
+
+                // Throttle updates to the UI based on UPDATE_INTERVAL_MS.
+                if (mTimeToNextUpdate < 0.0) {
+                    mTimeToNextUpdate = UPDATE_INTERVAL_MS;
+
+                    // Display data in TextViews. This must be done inside a
+                    // runOnUiThread call because
+                    // it affects the UI, which will cause an error if performed
+                    // from the Tango
+                    // service thread
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            //mRotationTextView.setText(rotationMsg);
+                            //mTranslationTextView.setText(translationMsg);
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onXyzIjAvailable(TangoXyzIjData arg0) {
+                // Ignoring XyzIj data
+            }
+
+            @Override
+            public void onTangoEvent(TangoEvent arg0) {
+                // Ignoring TangoEvents
+            }
+
+            @Override
+            public void onFrameAvailable(int arg0) {
+                // Ignoring onFrameAvailable Events
+
+            }
+
         });
     }
 
